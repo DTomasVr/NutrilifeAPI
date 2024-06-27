3dec05a7ada68c1af9ad8ddf944bb130f442954d
DTomasVr <diego.villafuerte.2033@gmail.com>
2024-06-27 18:38:33 -0500

    Rollback on Recipe

--- src/main/java/com/nutrilife/fitnessservice/controller/IngredientController.java
+++ src/main/java/com/nutrilife/fitnessservice/controller/IngredientController.java
@@ -1,8 +1,13 @@
 package com.nutrilife.fitnessservice.controller;
 
+import com.nutrilife.fitnessservice.mapper.RecipeMapper;
 import com.nutrilife.fitnessservice.model.dto.IngredientRequestDTO;
 import com.nutrilife.fitnessservice.model.dto.IngredientResponseDTO;
+import com.nutrilife.fitnessservice.model.dto.RecipeResponseDTO;
+import com.nutrilife.fitnessservice.model.entity.Recipe;
 import com.nutrilife.fitnessservice.service.IngredientService;
+import com.nutrilife.fitnessservice.service.RecipeService;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,8 @@ import java.util.List;
 public class IngredientController {
 
     private final IngredientService ingredientService;
+    private final RecipeService recipeService;
+    private final RecipeMapper recipeMapper;
 
     @GetMapping
     public ResponseEntity<List<IngredientResponseDTO>> getAllIngredients() {
@@ -39,7 +46,17 @@ public class IngredientController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteIngredient(@PathVariable Long id) {
+
+        List<Recipe> recipes = recipeService.getRecipesByIngredients(List.of(id));
+        List<RecipeResponseDTO> recipeDTOs = recipeService.convertToDTOListFromRecipe(recipes);
+
+        for (RecipeResponseDTO recipe : recipeDTOs) {
+            recipe.getIngredients().removeIf(ingredientId -> ingredientId.equals(id));
+            recipeService.updateRecipe(recipe.getId(), recipeMapper.convertToResponseDTO(recipe));
+        }
+
         ingredientService.deleteIngredient(id);
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -50,4 +67,9 @@ public class IngredientController {
         return ResponseEntity.ok(updatedIngredient);
     }
 
+    @GetMapping("/byName")
+    public ResponseEntity<List<IngredientResponseDTO>> findIngredientsByName(@RequestParam String name) {
+        List<IngredientResponseDTO> ingredients = ingredientService.findIngredientsByName(name);
+        return new ResponseEntity<>(ingredients, HttpStatus.OK);
+    }
 }
--- src/main/java/com/nutrilife/fitnessservice/repository/RecipeRepository.java
+++ src/main/java/com/nutrilife/fitnessservice/repository/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.nutrilife.fitnessservice.repository;
 
 import com.nutrilife.fitnessservice.model.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -15,4 +17,15 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findTop5ByOrderByScoreDesc();
 
     List<Recipe> findByType(String type);
+
+    List<Recipe> findByIngredients_IdIn(List<Long> ingredientIds);
+
+    @Query("SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE i.id IN :ingredientIds AND r.score >= 3.5")
+    List<Recipe> findPopularRecipesByIngredients(@Param("ingredientIds") List<Long> ingredientIds);
+
+    List<Recipe> findByTypeAndNutritionalGoal(String type, String nutritionalGoal);
+
+    @Query("SELECT DISTINCT r FROM Recipe r JOIN r.ingredients i WHERE r.nutritionalGoal = :nutritionalGoal AND i.id IN :ingredientIds")
+    List<Recipe> findByNutritionalGoalAndIngredients(@Param("nutritionalGoal") String nutritionalGoal,
+            @Param("ingredientIds") List<Long> ingredientIds);
 }
